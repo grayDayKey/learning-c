bf9c4672a5ff97c13d61c1ae206384cf6b5aeaa0
grayDayKey <8sergey.vyalkin8@gmail.com>
2018-02-07 16:30:04 +0700

    ch 1.4

--- ch 1/celsius-to-fahr.c
+++ ch 1/celsius-to-fahr.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 int main()
 {
     int celsius;
-    for (celsius = 0; celsius <= 300; celsius += 20)
+    for (celsius = LOWER; celsius <= UPPER; celsius += STEP)
         printf("%3d\t%6.1f\n", celsius, (9.0 / 5.0) * (celsius + 32.0));
     return 0;    
 }
--- ch 1/fahr-to-celsius.c
+++ ch 1/fahr-to-celsius.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 int main()
 {
     int fahr;
     printf("%3s\t%6s", "fahr", "celsius\n");
-    for (fahr = 0; fahr <= 300; fahr += 20)
-        printf("%3d\t%6.1f\n", fahr, (5.0 / 9./0) * (fahr - 32));
+    for (fahr = LOWER; fahr <= UPPER; fahr += STEP)
+        printf("%3d\t%6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
     return 0;
 }
