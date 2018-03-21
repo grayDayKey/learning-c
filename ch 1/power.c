b660e41f8a06c79b32b9aeef30bd00259fb08eac
grayDayKey <8sergey.vyalkin8@gmail.com>
2018-03-21 15:10:07 +0700

    ch 1.8

--- ch 1/power.c
+++ ch 1/power.c
@@ -12,9 +12,8 @@ int main()
 
 int power(int base, int n)
 {
-    int i, p;
-    p = 1;
-    for (i = 1; i <= n; ++i)
+    int p;
+    for (p = 1; n > 0; --n)
         p = p * base;
     return p;  
 }
