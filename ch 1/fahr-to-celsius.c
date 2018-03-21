b5b9d1a3c80752a0ee3cd2af9cd791e2a58d6a79
grayDayKey <8sergey.vyalkin8@gmail.com>
2018-03-21 14:27:01 +0700

    ch 1.7 homework

--- ch 1/fahr-to-celsius.c
+++ ch 1/fahr-to-celsius.c
@@ -4,11 +4,18 @@
 #define UPPER 300
 #define STEP 20
 
+float fahr_to_celsius(int);
+
 int main()
 {
     int fahr;
     printf("%3s\t%6s", "fahr", "celsius\n");
     for (fahr = LOWER; fahr <= UPPER; fahr += STEP)
-        printf("%3d\t%6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+        printf("%3d\t%6.1f\n", fahr, fahr_to_celsius(fahr));
     return 0;
+}
+
+float fahr_to_celsius(int fahr)
+{
+    return (5.0 / 9.0) * (fahr -32);
 }
--- ch 1/power.c
+++ ch 1/power.c
@@ -4,17 +4,17 @@ int power(int m, int n);
 
 int main()
 {
-  int i;
-  for (i =0; i < 10; ++i)
-    printf("%d %d %d\n", i, power(2, i), power(-3, i));
-  return 0;  
+    int i;
+    for (i =0; i < 10; ++i)
+        printf("%d %d %d\n", i, power(2, i), power(-3, i));
+    return 0;  
 }
 
 int power(int base, int n)
 {
-  int i, p;
-  p = 1;
-  for (i = 1; i <= n; ++i)
-    p = p * base;
-  return p;  
+    int i, p;
+    p = 1;
+    for (i = 1; i <= n; ++i)
+        p = p * base;
+    return p;  
 }
