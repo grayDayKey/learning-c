8fd0a079c4928de022434c104e31e705b3b3d1b1
grayDayKey <8sergey.vyalkin8@gmail.com>
2018-05-24 13:40:29 +0700

    1.5 fix

--- 1/1.5.c
+++ 1/1.5.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
+float celsius_to_fahr(float celsius);
+
 int main()
 {
-    int c;
-    while((c = getchar()) != EOF)
-        putchar(c);
-    return 0;    
+    float celsius;
+    printf("Celsius | Fahr\n");
+    for (celsius = UPPER; celsius >= LOWER; celsius -= STEP)
+        printf("%7.0f | %3.1f\n", celsius, celsius_to_fahr(celsius));
+    return 0;
+}
+
+float celsius_to_fahr(float celsius)
+{
+    return (9.0 * celsius) / 5.0 + 32.0;
 }
